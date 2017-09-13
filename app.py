26ad2eb3ce5f2b1f69c0016dd1f41b11b328200f
Angela Zalucha <amzalucha@gmail.com>
2017-09-13 18:16:51 +0000

    change port in app.py

--- app.py
+++ app.py
@@ -56,5 +56,5 @@ def stock():
 # With debug=True, Flask server will auto-reload 
 # when there are code changes
 if __name__ == '__main__':
-	app.run(port=5000, debug=True)
+	app.run(port=33507, debug=True)
 
