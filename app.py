40127c384e34787639ed0f07837bb7f541d1cdec
Angela Zalucha <amzalucha@gmail.com>
2017-09-16 01:20:42 +0000

    simple test plot

--- app.py
+++ app.py
@@ -29,15 +29,17 @@ def plot_close(stock):
     df=get_data(stock)
     close=pd.to_numeric(df['Close'])
     date=pd.to_datetime(df['Date'])
-    datelist = date.Date.tolist()
-    p=figure(x_axis_type='datetime')
+#    datelist = date.tolist()
+    x=range(10)
+    y=range(10)
     p = figure(tools="pan,wheel_zoom,box_zoom,reset",
               title='Data from Quandle WIKI set',
               x_axis_label='Date',
-              x_axis_type='datetime',
+#              x_axis_type='datetime',
               y_axis_label='Closing price')
 ##              x_range=(dt.datetime.now()-dt.timedelta(days=31),dt.datetime.now()))
-    p.line(date, close, line_width=2, legend=stock)
+#    p.line(date, close, line_width=2, legend=stock)
+     p.line(x,y,line_width=2, legend=stock)
     return p
 
 @app.route('/')
