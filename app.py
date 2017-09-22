e336cfa2a2b2b7efc8dbcd46346be23e341dfe87
Angela Zalucha <amzalucha@gmail.com>
2017-09-22 19:31:15 +0000

    angela's old version

--- app.py
+++ app.py
@@ -1,103 +1,66 @@
 from flask import Flask, render_template, request, redirect
-from bokeh.plotting import figure
-from bokeh.embed import components
+import pandas as pd
+import bokeh
+import dill
+import jinja2
+import pip
+import simplejson as json
 import requests
-import pandas
+#import json
+from bokeh.plotting import figure, show
+from bokeh.embed import components 
+#from datetime import datetime,timedelta
 import datetime as dt
-from StringIO import StringIO
-import random
 
-class DataLoadException(Exception):
-    pass
+app = Flask(__name__)
 
-def load_symbols(fn):
-    df = pandas.read_csv(fn)
-    return dict((r[1][5:], r[2].split(')')[0] + ')') for r in df.itertuples())
+#def get_data(stock):
+#    api_url = 'https://www.quandl.com/api/v1/datasets/WIKI/%s.json' % stock
+#    session = requests.Session()
+#    session.mount('http://', requests.adapters.HTTPAdapter(max_retries=3))
+#    raw_data = session.get(api_url)
+#    data = raw_data.json()
+#    column_names=data['column_names']
+#    ndata=data['data']
+#    df = pd.DataFrame(ndata, columns=column_names)
+#    return df
 
-def load_data(symbol, start_date, end_date):
-    url = 'https://www.quandl.com/api/v3/datasets/WIKI/%s.csv' % symbol.upper()
-    params = {'start_date': start_date.strftime('%Y-%m-%d'),
-              'end_date': end_date.strftime('%Y-%m-%d')}
-    response = requests.get(url, params=params)
-    print url, response.url, response.status_code
-    if response.status_code != requests.codes.ok:
-        raise DataLoadException('Server replied with status %i.' % response.status_code)
-    
-    return pandas.read_csv(StringIO(response.text), parse_dates=['Date'])
-
-def get_plot(symbol, days):
-    now = dt.datetime.now()
-    data = load_data(symbol, now - dt.timedelta(days), now)
-    
-    plot = figure(x_axis_type='datetime')
-    datelist = data.Date.tolist()
-    plot.patch(datelist + datelist[::-1], data.Low.tolist() + data.High.tolist()[::-1],
-               alpha=0.5, line_width=1, legend='High/Low')
-    plot.line(data.Date, data.Open, line_color='green', line_width=3, legend='Open', line_join='bevel')
-    plot.line(data.Date, data.Close, line_color='orange', line_width=3, legend='Close', line_join='bevel')
-    plot.yaxis.axis_label = 'Price'
-    plot.title = SYMBOLS[symbol]
-
+def plot_close(stock):
 #    df=get_data(stock)
 #    close=pd.to_numeric(df['Close'])
 #    date=pd.to_datetime(df['Date'])
 #    datelist = date.tolist()
-#    x=range(10)
-#    y=range(10)
-#    p = figure(tools="pan,wheel_zoom,box_zoom,reset",
-#              title='Data from Quandle WIKI set',
-#              x_axis_label='Date',
+    x=range(10)
+    y=range(10)
+    p = figure(tools="pan,wheel_zoom,box_zoom,reset",
+              title='Data from Quandle WIKI set',
+              x_axis_label='Date',
 #              x_axis_type='datetime',
-#              y_axis_label='Closing price')
+              y_axis_label='Closing price')
 ##              x_range=(dt.datetime.now()-dt.timedelta(days=31),dt.datetime.now()))
 #    p.line(date, close, line_width=2, legend=stock)
-#    p.line(x,y,line_width=2, legend=stock)
- 
-#    x=range(10)
-#    y=range(10)
-#    p = figure(tools="pan,wheel_zoom,box_zoom,reset",
-#              title='Data from Quandle WIKI set',
-#              x_axis_label='Date',
-#              x_axis_type='datetime',
-#              y_axis_label='Closing price')
-###              x_range=(dt.datetime.now()-dt.timedelta(days=31),dt.datetime.now()))
-#    p.line(date, close, line_width=2, legend=stock)
-#    p.line(x,y,line_width=2, legend=symbol)
-#    script, div = components(p)
-#    return script, div
-
-    return components(plot)
-
-def get_examples():
-    keys = random.sample(SYMBOLS.keys(), 3)
-    return ', '.join('<a href="/lookup?symbol=%s">%s</a>' % (k, SYMBOLS[k]) for k in keys)
-    
-
-app = Flask(__name__)
-SYMBOLS = load_symbols('WIKI-datasets-codes.csv')
+    p.line(x,y,line_width=2, legend=stock)
+    script, div = components(p)
+    return script, div
 
 @app.route('/')
 def index():
-    return render_template('index.html', symbol='', days=31, examples=get_examples())
+  return render_template('index.html')
 
-@app.route('/lookup')
-def lookup():
-    symbol = request.args.get('symbol', '').upper()
-    try:
-        days = int(request.args.get('days', '31'))
-    except ValueError:
-        days = 31
-    if symbol not in SYMBOLS:
-        return render_template('invalid.html', symbol=symbol, days=days, examples=get_examples())
-    
-    try:
-        script, div = get_plot(symbol, days)
-    except Exception as e:
-        # This could be due to problems in getting the data or plotting it
-        return render_template('error.html', symbol=symbol, days=days, error=e.message), 500
-    else:
-        return render_template('lookup.html', symbol=symbol, days=days, plot_script=script, plot_div=div)
+@app.route('/graph')#output
+def graph():
+#	stock = request.args.get("stock")
+#        stock=str(request.form['stock'])
+        stock = request.args.get('stock', '').upper()
 
+	# Create the plot
+	script, div = plot_close(stock)
+		
+	# Embed plot into HTML via Flask Render
+	return render_template("graph.html", script=script, div=div)
 
+# With debug=True, Flask server will auto-reload 
+# when there are code changes
 if __name__ == '__main__':
-    app.run(port=33507)
+	app.run(port=33507, debug=True)
+
